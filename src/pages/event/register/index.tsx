1ac29bd6398cba7dd23fbe149bc9c2887d23f9ef
devilzeros <devilzeros@outlook.co.th>
2024-10-05 15:10:06 +0700

    est

---
+++ src/pages/event/register/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "@/components/layout";
+
+
+const RegisterPage: React.FC = (props) => {
+    return (
+        <Layout>
+            <div className="">
+                
+            </div>
+        </Layout>
+    );
+}
+export default RegisterPage;
