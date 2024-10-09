046dafb11145795e8f642c5ced8938e29ad711da
devilzeros <devilzeros@outlook.co.th>
2024-10-09 14:10:30 +0700

    est

--- src/containers/Home/Slider.tsx
+++ src/containers/Home/Slider.tsx
@@ -12,95 +12,34 @@ const HomeSlider: React.FC = () => {
         cssEase: "linear"
     };
     return (
-        <div className="slide-style ">
-            <div className="slider-container overflow-hidden">
-                <Slider {...settings}>
-                    <div className="slide ">
-                        <div className="relative h-screen flex items-center justify-center bg-black" style={{ height: 'calc(100vh - 6rem)' }}>
-                            <div className="absolute inset-0 overflow-hidden">
-                                <img
-                                    src="/images/bg01.png"
-                                    alt="Fantasy Event"
-                                    className="object-cover w-full h-full opacity-60"
-                                />
-                            </div>
+        <div className="relative h-screen flex items-center justify-center bg-black" style={{ height: 'calc(100vh - 6rem)' }}>
+            <div className="absolute inset-0 overflow-hidden">
+                <img
+                    src="/images/bg01.png"
+                    alt="Fantasy Event"
+                    className="object-cover w-full h-full opacity-60"
+                />
+            </div>
 
-                            <div className="relative z-10 text-center">
-                                <h1 className="text-5xl font-bold title-gold">
-                                    Welcome to Me Guild
-                                </h1>
-                                <p className="mt-4 text-xl title-gold">
-                                    Join our world of fantasy and adventure!
-                                </p>
-                                <div className="mt-8">
-                                    <button
-                                        className="relative text-white py-2 px-6 rounded-lg bg-cover bg-center bg-[url('/images/btnBlue.png')] hover:bg-[url('/images/btnRed.png')] hover:opacity-90 w-[125px]"
-                                    // style={{ backgroundImage: "url('/images/btnBlue.png')" }}
-                                    >
-                                        <span className="relative z-10">Join Discord</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="slide ">
-                        <div className="relative h-screen flex items-center justify-center bg-black" style={{ height: 'calc(100vh - 6rem)' }}>
-                            <div className="absolute inset-0 overflow-hidden">
-                                <img
-                                    src="/images/bg01.png"
-                                    alt="Fantasy Event"
-                                    className="object-cover w-full h-full opacity-60"
-                                />
-                            </div>
-
-                            <div className="relative z-10 text-center">
-                                <h1 className="text-5xl font-bold title-gold">
-                                    Welcome to Me Guild
-                                </h1>
-                                <p className="mt-4 text-xl title-gold">
-                                    Join our world of fantasy and adventure!
-                                </p>
-                                <div className="mt-8">
-                                    <button
-                                        className="relative text-white py-2 px-6 rounded-lg bg-cover bg-center bg-[url('/images/btnBlue.png')] hover:bg-[url('/images/btnRed.png')] hover:opacity-90 w-[125px]"
-                                    // style={{ backgroundImage: "url('/images/btnBlue.png')" }}
-                                    >
-                                        <span className="relative z-10">Join Discord</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="slide ">
-                        <div className="relative h-screen flex items-center justify-center bg-black" style={{ height: 'calc(100vh - 6rem)' }}>
-                            <div className="absolute inset-0 overflow-hidden">
-                                <img
-                                    src="/images/bg01.png"
-                                    alt="Fantasy Event"
-                                    className="object-cover w-full h-full opacity-60"
-                                />
-                            </div>
-
-                            <div className="relative z-10 text-center">
-                                <h1 className="text-5xl font-bold title-gold">
-                                    Welcome to Me Guild
-                                </h1>
-                                <p className="mt-4 text-xl title-gold">
-                                    Join our world of fantasy and adventure!
-                                </p>
-                                <div className="mt-8">
-                                    <button
-                                        className="relative text-white py-2 px-6 rounded-lg bg-cover bg-center bg-[url('/images/btnBlue.png')] hover:bg-[url('/images/btnRed.png')] hover:opacity-90 w-[125px]"
-                                    // style={{ backgroundImage: "url('/images/btnBlue.png')" }}
-                                    >
-                                        <span className="relative z-10">Join Discord</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </Slider>
+            <div className="relative z-10 text-center">
+                <div className="relative z-10 text-center">
+                    <h1 className="text-5xl font-bold text-gold" data-text="Welcome to Me Guild">
+                        Welcome to Me Guild
+                    </h1>
+                </div>
+                <div className="relative z-10 text-center">
+                    <h1 className="text-5xl font-bold text-gold" data-text="Join our world of fantasy and adventure!">
+                        Join our world of fantasy and adventure!
+                    </h1>
+                </div>
+                <div className="mt-8">
+                    <button
+                        className="relative text-white py-2 px-6 rounded-lg bg-cover bg-center bg-[url('/images/btnBlue.png')] hover:bg-[url('/images/btnRed.png')] hover:opacity-90 w-[125px]"
+                    // style={{ backgroundImage: "url('/images/btnBlue.png')" }}
+                    >
+                        <span className="relative z-10">Join Discord</span>
+                    </button>
+                </div>
             </div>
         </div>
     );
