0233269136cfb2e6c4df9a0ba8fc7125c8fdb3d2
Ploy <117616170+yaminP@users.noreply.github.com>
2024-10-10 18:20:17 +0700

    updateprisma-newscard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import ActivityUpdate from "@/containers/Home/ActivtiesUpdate";
 import GuildRecomend from "@/containers/Home/GuildRecomend";
+import NewsCard from "@/containers/Home/NewsCard";
 import NewsUpdate from "@/containers/Home/NewsUpdate";
 import PoppularPerson from "@/containers/Home/PopularPerson";
 import HomeSlider from "@/containers/Home/Slider";
@@ -12,10 +13,11 @@ const Home: React.FC = (props) => {
   return (
     <Layout>
       <section className="">
-        <HomeSlider/>
+        <HomeSlider />
+        <NewsCard />
+        <NewsUpdate />
         <ActivityUpdate />
         {/* <GuildRecomend /> กิลด์แนะนำ */}
-        <NewsUpdate />
         {/* <PoppularPerson/> */}
       </section>
     </Layout>
