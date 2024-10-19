28b4e39d4aef532c05ef23ae8741433a9f756d58
Ploy <117616170+yaminP@users.noreply.github.com>
2024-10-19 14:36:00 +0700

    acc

---
+++ src/containers/Activity/LatsetMath.tsx
@@ -0,0 +1,14 @@
+const LatestMath: React.FC = () => {
+    return (
+        <section className="my-10">
+            <div className="flex items-center justify-center">
+                <div className="w-16 border-t-4 border-gray-600"></div>
+                <h2 className="mx-4 text-red-600 text-2xl font-bold whitespace-nowrap">
+                    LATEST <span className="text-white">MATCHES</span>
+                </h2>
+                <div className="flex-grow border-t-4 border-gray-600"></div>
+            </div>
+        </section>
+    )
+}
+export default LatestMath;
--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout"
+import LatestMath from "@/containers/Activity/LatsetMath";
 import LatestNews from "@/containers/CardReccommend/LatesNews";
 import LatestActivity from "@/containers/CardReccommend/LatestActivity";
 import SearchBar from "@/containers/CardReccommend/SearchBarForm";
 import WeAreSocial from "@/containers/CardReccommend/WeAreSocial";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { BiArrowFromRight } from "react-icons/bi";
 
 const ActivityPage: React.FC = (props) => {
 
@@ -49,30 +51,43 @@ const ActivityPage: React.FC = (props) => {
                 </div>
 
                 <div className="container mx-auto px-2 md:px-10 xl:px-0 py-10">
+                    <a href="/" className='text-gray-100 flex items-center gap-2 hover:underline decoration-sky-500'> <BiArrowFromRight /> Home</a>
+
+                    <p className="text-4xl font-bold uppercase flex text-gray-100 border-teal-400 border-b-4 pb-2 mt-10">
+                        <span>
+                            Activity
+                            <span className="text-xs bg-sky-400 px-1 rounded-md text-white font-light ml-2">Update</span>
+                        </span>
+                    </p>
                     <div className="lg:grid grid-cols-12 gap-10 mt-6">
-                        <div className="col-span-8 bg-black/10 p-5">
-                            <ul className="list-inside text-sm ">
-                                {loading ? (
-                                    <p>Loading...</p>
-                                ) : (
-                                    activityData.map(activities => (
-                                        <li key={activities.id} className="border-b border-slate-500 mb-2">
-                                            <Link href={`/activity/${activities.id}`} className="flex justify-between text-white hover:text-cyan-500">
-                                                {activities.title}
-                                                <span className="text-orange-400">{activities.point} point</span>
-                                            </Link>
-                                        </li>
-                                    ))
-                                )}
-                            </ul>
+                        <div className="col-span-8">
+                            <div>
+                                <ul className="list-inside text-sm bg-black/10 p-5">
+                                    {loading ? (
+                                        <p>Loading...</p>
+                                    ) : (
+                                        activityData.map(activities => (
+                                            <li key={activities.id} className="border-b border-slate-500 mb-2">
+                                                <Link href={`/activity/${activities.id}`} className="flex justify-between text-white hover:text-cyan-500">
+                                                    {activities.title}
+                                                    <span className="text-orange-400">{activities.point} point</span>
+                                                </Link>
+                                            </li>
+                                        ))
+                                    )}
+                                </ul>
+                            </div>
+                            <div>
+                                <LatestMath />
+                            </div>
                         </div>
                         <div className='col-span-4'>
                             {/* search */}
-                            <SearchBar/>
+                            <SearchBar />
                             {/* Social */}
                             <WeAreSocial />
                             {/* Latest News */}
-                            <LatestNews/>
+                            <LatestNews />
                             {/* Latest Match */}
                         </div>
                     </div>
