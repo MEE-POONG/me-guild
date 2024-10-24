fc0a673a1df223493acc818702fe2618a30b9035
devilzeros <devilzeros@outlook.co.th>
2024-10-24 15:13:48 +0700

    est

--- src/pages/guild/list.tsx
+++ src/pages/guild/list.tsx
@@ -1,10 +1,11 @@
 import SecondaryTopicOne from "@/components/Head/SecondaryTopicOne";
 import Layout from "@/components/Layout";
+import LatestNews from "@/containers/CardReccommend/LatestNews";
+import SearchBar from "@/containers/CardReccommend/SearchBarForm";
+import WeAreSocial from "@/containers/CardReccommend/WeAreSocial";
 import GuildList from "@/containers/Guild/GuildList";
-import PeopleList from "@/containers/Guild/PeopleList";
-import Link from "next/link";
 import React from "react"
-import { GiDaemonSkull } from "react-icons/gi";
+import { BiArrowFromRight } from "react-icons/bi";
 
 const GuildListPage: React.FC = (props) => {
     const backgroundImageUrl = '/images/grandhall2.png';
@@ -13,7 +14,28 @@ const GuildListPage: React.FC = (props) => {
         <Layout>
             <section className="">
                 <SecondaryTopicOne title={`Guild Hall`} imgBg={backgroundImageUrl} />
-                <GuildList />
+                <div className="container mx-auto px-2 md:px-10 xl:px-0 py-3">
+                    <a href="/" className='text-gray-100 flex items-center gap-2 hover:underline decoration-sky-500'> <BiArrowFromRight /> Home</a>
+
+                    <h3 className="text-4xl font-bold uppercase flex text-gray-100 border-teal-400 border-b-4 pb-2 mt-10">
+                        Goild
+                    </h3>
+                    <div className="lg:grid grid-cols-12 gap-10 mt-6">
+                        <div className="col-span-8">
+                            <GuildList />
+                        </div>
+
+                        <div className='col-span-4'>
+                            {/* search */}
+                            <SearchBar />
+                            {/* Social */}
+                            <WeAreSocial />
+                            {/* Latest News */}
+                            <LatestNews />
+                            {/* Latest Match */}
+                        </div>
+                    </div>
+                </div>
             </section>
         </Layout>
     )
